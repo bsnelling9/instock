a7f821c0f2cb86480cf9495da7f1595cad5493eb
Brodie <brodiesnelling@gmail.com>
2022-07-24 10:31:36 -0400

    updated code

--- client/src/components/EditInventory/EditInventory.jsx
+++ client/src/components/EditInventory/EditInventory.jsx
@@ -2,7 +2,7 @@ import "./EditInventory.scss";
 import { Component } from "react";
 import arrowBack from "../../assets/Icons/arrow_back-24px.svg"
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 export default class EditInventory extends Component {
 
@@ -107,7 +107,13 @@ export default class EditInventory extends Component {
         return(
         <div className="editInventory">
             <div className="editInventoryHeader">
-                <img className="editInventoryHeader__icon" src={arrowBack} onClick={(event) => (window.location.href = `/inventory/${this.state.id}`)} alt="arrow back"></img>
+                <Link className="addInventoryHeader__link" to={"/inventory"}>
+                    <img
+                        className="addInventoryHeader__icon"
+                        src={arrowBack}
+                        alt="arrow back"
+                    />
+                </Link>
                 <h1 className="editInventoryHeader__heading">edit inventory item</h1>
             </div>
             <div className="editInventoryForm">
